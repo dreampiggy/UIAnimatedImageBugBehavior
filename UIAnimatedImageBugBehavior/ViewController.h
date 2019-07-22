6a2474952d1b1a5f45a3fce0901ec682a29077a3
DreamPiggy <lizhuoli1126@126.com>
2019-07-22 17:58:49 +0800

    Initial Commit

---
+++ UIAnimatedImageBugBehavior/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIAnimatedImageBugBehavior
+//
+//  Created by lizhuoli on 2019/7/22.
+//  Copyright © 2019 lizhuoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIAnimatedImageBugBehavior/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  UIAnimatedImageBugBehavior
+//
+//  Created by lizhuoli on 2019/7/22.
+//  Copyright © 2019 lizhuoli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
